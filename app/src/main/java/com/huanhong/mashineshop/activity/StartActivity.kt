6d09ef8a7b52b7da36571b49fdfc635380233b0d
Chen Binbin <chenbinbin@nissanchina.cn>
2019-06-04 09:50:21 +0800

    commit

--- app/src/main/java/com/huanhong/mashineshop/activity/StartActivity.kt
+++ app/src/main/java/com/huanhong/mashineshop/activity/StartActivity.kt
@@ -1,6 +1,8 @@
 package com.huanhong.mashineshop.activity
 
 import android.content.Intent
+import com.alibaba.sdk.android.push.CommonCallback
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory
 import com.bumptech.glide.Glide
 import com.huanhong.mashineshop.BaseActivity
 import com.huanhong.mashineshop.R
@@ -30,6 +32,15 @@ class StartActivity:BaseActivity(){
         iv_.setOnClickListener{
             startActivity(Intent(this@StartActivity,GoodsNumberActivity::class.java))
         }
+        unbind()
+    }
+
+    private fun unbind() {
+        PushServiceFactory.getCloudPushService().unbindAccount(object : CommonCallback {
+            override fun onSuccess(s: String) {
+            }
+            override fun onFailed(s: String, s1: String) {}
+        })
 
     }
 }
--- app/src/main/java/com/huanhong/mashineshop/activity/WaitForPhoneConfirmActivity.java
+++ app/src/main/java/com/huanhong/mashineshop/activity/WaitForPhoneConfirmActivity.java
@@ -4,15 +4,18 @@ import android.content.Intent;
 import android.view.MotionEvent;
 import android.view.View;
 
+import com.alibaba.sdk.android.push.CommonCallback;
+import com.alibaba.sdk.android.push.noonesdk.PushServiceFactory;
 import com.huanhong.mashineshop.BaseActivity;
 import com.huanhong.mashineshop.R;
 import com.huanhong.mashineshop.ReceiveEvent;
+import com.huanhong.mashineshop.utils.SharedPreferencesUtils;
 
 import org.greenrobot.eventbus.EventBus;
 import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
-public class WaitForPhoneConfirmActivity extends BaseActivity{
+public class WaitForPhoneConfirmActivity extends BaseActivity {
     @Override
     protected int getContentViewId() {
         return R.layout.activity_wait_for_phone;
@@ -32,20 +35,21 @@ public class WaitForPhoneConfirmActivity extends BaseActivity{
         findViewById(R.id.skip).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(WaitForPhoneConfirmActivity.this,StartGameActivity.class));
+                startActivity(new Intent(WaitForPhoneConfirmActivity.this, StartGameActivity.class));
             }
         });
     }
-/*
-* GameSuccess  | [string]  | 游戏成功
-GameFail  | [string]  | 游戏失败
-GameStart  | [string]  | 游戏开始
-* */
+
+    /*
+    * GameSuccess  | [string]  | 游戏成功
+    GameFail  | [string]  | 游戏失败
+    GameStart  | [string]  | 游戏开始
+    * */
     @Subscribe(threadMode = ThreadMode.MAIN)
-    public void receive(ReceiveEvent event){
+    public void receive(ReceiveEvent event) {
         // 收到确认消息 跳转游戏
-        if("GameStart".equals(event.title)){
-            startActivity(new Intent(WaitForPhoneConfirmActivity.this,StartGameActivity.class));
+        if ("GameStart".equals(event.title)) {
+            startActivity(new Intent(WaitForPhoneConfirmActivity.this, StartGameActivity.class));
         }
     }
 }
